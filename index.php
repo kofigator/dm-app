b9a98036373a82e3e4ec9f2c21e483f9bf541834
kofigator <akuprince50@gmail.com>
2023-04-28 11:34:40 +0000

    made change to index.php, registration.html and dashboard.html.  The changes done was adding bootstrap to those pages

--- index.php
+++ index.php
@@ -1,38 +1,34 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<link rel="stylesheet" href="../CSS/main_style.css">-->
-
-    <title>Login Page</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
+   <title>Login Page</title>
 </head>
-
 <body>
+   <div class="container mt-5">
+      <div class="row justify-content-center">
+         <div class="col-lg-4 col-md-6 col-sm-8">
+            <h3 class="text-center mb-4">Login</h3>
+            <form action="" method="" id="Login-Form">
+               <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="text" class="form-control" id="email" name="email">
+               </div>
+               <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="password" name="password">
+               </div>
+               <div class="d-grid gap-2">
+                  <button type="submit" class="btn btn-primary">Login</button>
+               </div>
+            </form>
+         </div>
+      </div>
+   </div>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
 
-    <!--Background-->
-    <div id="bg"></div>
-
-    <!--log in fields-->
-    <form id="Login-Form" method="post">
-        <div class="form-field">
-            <input type="email" name="email" id="email" placeholder="Email / Username" required />
-        </div>
-
-        <div class="form-field">
-            <input type="password" name="password" id="password" placeholder="Password" required />
-        </div>
-
-        <div class="form-field">
-
-            <!-- <button class="btn" type="submit">Log in</button> -->
-            <button type="submit" class="btn">Log in</button>
-
-            <button class="btn" onclick="window.location.href='registration.html';">Register</button>
-        </div>
-    </form>
 
     <script src="js/jquery-3.6.0.min.js"></script>
     <script>
