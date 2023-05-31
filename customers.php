d8cbc57a5a2f5b12d411cdbf8194e76cd41afec3
Anlimah <y.m.ratty7@gmail.com>
2023-05-31 19:36:55 +0000

    customer retrieval

--- customers.php
+++ customers.php
@@ -14,88 +14,93 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
     <title>Login Page</title>
     <style>
-      
-      .body>form {
-          height: 100% !important;
-          width: 100% !important;
-      }
+        .body>form {
+            height: 100% !important;
+            width: 100% !important;
+        }
 
-      form {
-          max-width: 500px;
-      }
+        form {
+            max-width: 500px;
+        }
 
-      #tableBody td{
-          padding-right: 40px;
-          padding-bottom: 20px;
-      }
+        #tableBody td {
+            padding-right: 40px;
+            padding-bottom: 20px;
+        }
 
-      .act_btn td{
-        padding: 20px;
-      }
+        .act_btn td {
+            padding: 20px;
+        }
 
-      ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      overflow: hidden;
-      background-color: #333;
-      }
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
 
-      li {
-      float: left;
-      }
+        li {
+            float: left;
+        }
 
-      li a, .dropbtn {
-      display: inline-block;
-      color: white;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      }
+        li a,
+        .dropbtn {
+            display: inline-block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
 
-      li a:hover, .dropdown:hover .dropbtn {
-      background-color: rgb(236, 195, 195);
-      }
+        li a:hover,
+        .dropdown:hover .dropbtn {
+            background-color: rgb(236, 195, 195);
+        }
 
-      .dropdown-content {
-      display: none;
-      position: absolute;
-      background-color: #f9f9f9;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-      }
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
 
-      .dropdown-content a {
-      color: black;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-      text-align: left;
-      }
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
 
-      .dropdown-content a:hover {background-color: #f1f1f1;}
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
 
-      .dropdown:hover .dropdown-content {
-      display: block;
-      }
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
 
-      #customer_header{
-      margin: 5px 10px;
-      color: #fff;
-      
-      }
+        #customer_header {
+            margin: 5px 10px;
+            color: #fff;
+
+        }
     </style>
 </head>
 
 <body class="fluid-container">
     <nav>
         <ul>
-        <li class="dropdown">
-              <a href="add_customer.php" class="dropbtn"><img src="add.jpg" alt="" width="35px" height="35px"></a>
-              
+            <li class="dropdown">
+                <a href="add_customer.php" class="dropbtn"><img src="add.jpg" alt="" width="35px" height="35px"></a>
+
             </li>
-            <li id="customer_header"><h1>CUSTOMERS</h1></li>
+            <li id="customer_header">
+                <h1>CUSTOMERS</h1>
+            </li>
         </ul>
     </nav>
 
@@ -103,14 +108,14 @@
         <table>
             <thead>
                 <tr>
-                  <th>Name</th>
-                  <th>Phone No.</th>
-                </tr>   
+                    <th>Name</th>
+                    <th>Phone No.</th>
+                </tr>
             </thead>
             <tbody id="tableBody">
-            <?php 
-                include 'retrieve_customers.php'; 
-            ?>
+                <?php
+                include 'retrieve_customers.php';
+                ?>
             </tbody>
         </table>
     </div>
