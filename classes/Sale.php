8e9ee810420fa85c979652226ce2b2a3101d0787
kofigator <akuprince50@gmail.com>
2023-06-13 14:11:20 +0000

    updated the reports.php and also the database.sql

--- classes/Sale.php
+++ classes/Sale.php
@@ -4,6 +4,7 @@ require_once('DatabaseMethods.php');
 class Sale
 {
     private $db;
+
     public function __construct()
     {
         $this->db = new DatabaseMethods();
@@ -12,36 +13,42 @@ class Sale
     public function getListOfItemsByTextInput($data, $user_id)
     {
         $query = "SELECT `item_id`, `item_name`, `unit_price` 
-                    FROM items WHERE u_id = :ui AND item_name LIKE '%{$data}%'";
-        return $this->db->getData($query, array(":ui" => $user_id));
+                  FROM items WHERE u_id = :ui AND item_name LIKE :data";
+        $params = array(":ui" => $user_id, ":data" => "%{$data}%");
+        return $this->db->getData($query, $params);
     }
 
     public function getListOfItemsByID($itemID, $user_id)
     {
         $query = "SELECT `item_id`, `item_name`, `unit_price` 
-                    FROM items WHERE u_id = :ui AND item_id = :it";
-        return $this->db->getData($query, array("it" => $itemID, ":ui" => $user_id));
+                  FROM items WHERE u_id = :ui AND item_id = :it";
+        $params = array(":ui" => $user_id, ":it" => $itemID);
+        return $this->db->getData($query, $params);
     }
 
     public function sellProducts($data, $user_id)
     {
         /*
         INSERT INTO `customers`(`cust_id`, `u_id`, `name`, `number`, `gender`, `address`) 
-VALUES (1, 1, 'Non customer', '0123456789', 'none', 'none')
+        VALUES (1, 1, 'Non customer', '0123456789', 'none', 'none')
         */
         $totalAdded = 0;
-        foreach ($data["items"] as $item) {
-            $query = "INSERT INTO `sales`(`item_id`, `cust_id`, `user_id`, `quantity`, `unit_price`) 
-                    VALUES(:ii, :ci, :ui, :qt, :up)";
-            return $item;
-            $totalAdded += $this->db->inputData($query, array(
-                ":ii" => $item["id"],
-                ":ii" => $data["customer-list"],
-                ":ui" => $user_id,
-                ":qt" => $item["quantity"],
-                ":up" => $item["unit_price"]
-            ));
+        
+        if (isset($data["items"]) && is_array($data["items"])) {
+            foreach ($data["items"] as $item) {
+                $query = "INSERT INTO `sales` (`item_id`, `cust_id`, `user_id`, `quantity`, `unit_price`) 
+                          VALUES(:ii, :ci, :ui, :qt, :up)";
+                $params = array(
+                    ":ii" => $item["id"],
+                    ":ci" => $data["customer-list"],
+                    ":ui" => $user_id,
+                    ":qt" => $item["quantity"],
+                    ":up" => $item["unit_price"]
+                );
+                $totalAdded += $this->db->inputData($query, $params);
+            }
         }
+        
         return $totalAdded;
     }
 }
--- reports.php
+++ reports.php
@@ -1,11 +1,6 @@
 <?php
 session_start();
 if (!isset($_SESSION["user"])) header("Location: index.php");
-if (isset($_GET["logout"])) {
-    session_unset();
-    session_destroy();
-    header("Location: index.php");
-}
 require_once('classes/Inventory.php');
 $Inventory = new Inventory();
 ?>
@@ -16,143 +11,76 @@ $Inventory = new Inventory();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<link rel="stylesheet" href="../CSS/main_style.css">-->
+    <title>Reports</title>
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <!-- MDB -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
-    <title>Reports</title>
-
-    <style>
-        .body>form {
-            height: 100% !important;
-            width: 100% !important;
-        }
-
-        form {
-            max-width: 500px;
-        }
-
-        #tableBody td {
-            padding-right: 40px;
-            padding-bottom: 20px;
-        }
-
-        .act_btn td {
-            padding: 20px;
-        }
-
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: #333;
-        }
-
-        li {
-            float: left;
-        }
-
-        li a,
-        .dropbtn {
-            display: inline-block;
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-        }
-
-        li a:hover,
-        .dropdown:hover .dropbtn {
-            background-color: rgb(236, 195, 195);
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-            text-align: left;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #f1f1f1;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        #item_header {
-            margin: 15px 50px;
-            color: #fff;
-
-        }
-
-        header {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 10px;
-        }
-
-        header>h1 {
-            text-align: center !important;
-        }
-
-        .dashboard-item {
-            border-radius: 15px;
-            text-align: center;
-            font-size: 18px;
-        }
-
-        .back {
-            cursor: pointer;
-        }
-
-        .add-new-element {
-            cursor: pointer;
-            position: absolute;
-            right: 15px;
-            font-size: 50px;
-            border-radius: 50px;
-            color: green;
-            bottom: 0px;
-        }
-
-        .search-list .list-item {
-            cursor: pointer;
-        }
-    </style>
 </head>
 
 <body class="fluid-container">
-    <header style="position: relative !important; width: 100% !important; height: 60px !important; display: flex; justify-content: center; align-items: center">
-        <span style="flex-grow: 1" class="bi bi-arrow-left back" style="color: #fff !important; font-size: 26px !important"></span>
-        <h1 style="flex-grow: 8">REPORTS</h1>
-        <?php require_once('inc/header.php') ?>
+
+    <div class="container mt-4">
+    <header style="position: relative !important; width: 100% !important; height: 60px !important;">
+        <h1>Reports</h1>
+        <a href="logout.php" style="position: absolute; top: 10px; right: 10px; color: #fff; text-decoration: none;">
+            Logout
+        </a>
     </header>
 
-    <div class="payment-type">
-        <select name="payment-type" id="payment-type">
-            <option value=""></option>
-            <option value="cash">CASH</option>
-            <option value="momo">MOMO</option>
-        </select>
-    </div>
+        <!-- Filter Form -->
+        <form id="reports-form" method="post">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <label for="reportType">Report Type</label>
+                    <select class="form-select" id="reportType" name="reportType">
+                        <option value="inventory">Inventory</option>
+                    </select>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="startDate">Start Date</label>
+                    <input type="date" class="form-control" id="startDate" name="startDate">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="endDate">End Date</label>
+                    <input type="date" class="form-control" id="endDate" name="endDate">
+                </div>
+            </div>
 
+
+            <button type="submit" class="btn btn-primary">Generate Report</button>
+        </form>
+
+        <!-- Report Results Table -->
+        <div class="mt-4">
+            <h2>Report Results</h2>
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Report Type</th>
+                        <th>Date</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Table rows with report data will be generated dynamically -->
+                    <tr>
+                        <td>1</td>
+                        <td>John Doe</td>
+                        <td>Inventory</td>
+                        <td>2023-05-01</td>
+                        <td>$500</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <script src="js/jquery-3.6.0.min.js"></script>
     <!-- MDB -->
@@ -160,8 +88,26 @@ $Inventory = new Inventory();
     <script>
         $(document).ready(function() {
 
-            $(".back").click(function() {
-                window.location.href = "dashboard.php";
+            $("#reports-form").on("submit", function(e) {
+                e.preventDefault();
+
+                $.ajax({
+                    type: "POST",
+                    url: "api/generate-report",
+                    data: new FormData(this),
+                    contentType: false,
+                    cache: false,
+                    processData: false,
+                }).done(function(data) {
+                    console.log(data);
+                    if (data.success) {
+                        window.location.href = "reports.php";
+                    }
+                    alert(data.message);
+                    return;
+                }).fail(function(error) {
+                    console.log(error);
+                })
             });
 
         });
